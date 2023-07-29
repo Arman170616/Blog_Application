ff4be6c9d9bb603af82046ae6413a3f7bc0a9015
Arman170616 <armanice09@gmail.com>
2023-07-29 22:33:33 +0600

    Createview with forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 
 class BlogListView(ListView):
@@ -11,3 +12,9 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
