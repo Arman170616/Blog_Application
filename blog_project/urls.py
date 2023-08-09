81f286f326c4f04e90800eb8bc2c5d3bce6f9fc5
Arman170616 <armanice09@gmail.com>
2023-08-09 22:16:20 +0600

    signup functionality added

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -4,5 +4,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), # new
     path('', include('blog.urls')), 
+    path('accounts/', include('accounts.urls')), # new
+
 ]
